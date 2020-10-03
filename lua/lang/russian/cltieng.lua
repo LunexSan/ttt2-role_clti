650d2d1423bcc4fa5fe963fabc0ee34233ed8ede
Blueberryy <igory.ygr200@gmail.com>
2020-10-03 12:30:10 +0500

    Create cltieng.lua

---
+++ lua/lang/russian/cltieng.lua
@@ -0,0 +1,12 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[CULTIST.name] = "Культист"
+L["info_popup_" .. CULTIST.name] = [[Вы культист, работайте со своим культом!]]
+L["body_found_" .. CULTIST.abbr] = "Он был культистом."
+L["search_role_" .. CULTIST.abbr] = "Этот человек был культистом!"
+L["target_" .. CULTIST.name] = "Культист"
+L["ttt2_desc_" .. CULTIST.name] = [[Культист - обычный член культа, помогает культу добиться победы и устраняет всех, кто противостоит вам!]]
+
+
+-- OTHER ROLE LANGUAGE STRINGS
